6a3b67ca8cdb3bf53a5be5d5c7a95767a911d905
ETTIFOURI El Hassane <h.ettifouri@gmail.com>
2024-01-12 21:53:14 +0100

    Init Projet Maven

---
+++ gsproduit/src/main/java/ma/eheio/gsproduit/App.java
@@ -0,0 +1,13 @@
+package ma.eheio.gsproduit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
